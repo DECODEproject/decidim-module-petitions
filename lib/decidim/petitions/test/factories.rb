4158cdd2bc569b8e9d61ab1105dcfd223e324180
Mijail Rondon <mijail@riseup.net>
2019-06-24 14:34:38 -0500

    Fix petition factory

--- lib/decidim/petitions/test/factories.rb
+++ lib/decidim/petitions/test/factories.rb
@@ -17,6 +17,7 @@ FactoryBot.define do
     author { build(:user, :confirmed, :admin, organization: component.organization) }
     component { build(:component, manifest_name: "petitions") }
     instructions_url { Faker::Internet.url }
+    image { Decidim::Dev.test_file("city2.jpeg", "image/jpeg") }
     json_schema do
       {
         "mandatory": [
@@ -47,8 +48,8 @@ FactoryBot.define do
     json_attribute_info do
       [
         {
-          "name": "codes",
-          "type": "str",
+          name: "codes",
+          type: "str",
           value_set: %w(eih5O nuu3S Pha6x lahT4 Ri3ex Op2ii EG5th ca5Ca TuSh1 ut0iY Eing8 Iep1H yei2A ahf3I Oaf8f nai1H aib5V ohH5v eim2E Nah5l ooh5C Uqu3u Or2ei aF9fa ooc8W)
         }
       ]
@@ -57,15 +58,29 @@ FactoryBot.define do
     json_attribute_info_optional do
       [
         {
-          "name": "codes",
-          "type": "str",
-          value_set: %w(eih5O nuu3S Pha6x lahT4 Ri3ex Op2ii EG5th ca5Ca TuSh1 ut0iY Eing8 Iep1H yei2A ahf3I Oaf8f nai1H aib5V ohH5v eim2E Nah5l ooh5C Uqu3u Or2ei aF9fa ooc8W)
+          k: 2,
+          name: "age",
+          type: "str",
+          value_set: ["0-19", "20-29", "30-39", ">40"]
+        },
+        {
+          k: 2,
+          name: "gender",
+          type: "str",
+          value_set: %w(F M O)
+        },
+        {
+          k: 2,
+          name: "district",
+          type: "str",
+          value_set: %w(1 2 3 4 5 6 7 8 9 10)
         }
       ]
     end
 
     trait :open do
       open { true }
+      state { "opened" }
     end
   end
 end
