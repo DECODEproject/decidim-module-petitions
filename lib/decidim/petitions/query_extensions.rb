0f30638147e98ab14c452917b03604649ca0183f
Mijail Rondon <mijail@riseup.net>
2019-06-27 17:34:12 -0500

    Change resolution using current organization context

--- lib/decidim/petitions/query_extensions.rb
+++ lib/decidim/petitions/query_extensions.rb
@@ -5,19 +5,23 @@ module Decidim
     module QueryExtensions
       def self.define(type)
         type.field :petition do
-          type !PetitionType
+          type PetitionType
 
           argument :id, !types.ID, "The petition ID"
 
-          resolve lambda { |_obj, args, _ctx|
-            Petition.find_by(id: args[:id])
+          resolve lambda { |_obj, args, ctx|
+            petition = Petition.find_by(id: args[:id])
+            return nil unless petition&.organization == ctx[:current_organization]
+            petition
           }
         end
 
         type.field :petitions do
           type types[PetitionType]
-          resolve lambda { |_obj, _args, _ctx|
-            Petition.all
+          resolve lambda { |_obj, _args, ctx|
+            Petition.all.select do |petition|
+              petition.organization == ctx[:current_organization]
+            end
           }
         end
       end
