c604953a48e191559f06226572c499f2b36a8918
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2019-11-08 17:50:36 +0100

    Adds URLs on default JSON config (#30)
    * Adds DECODE instructions on README
    
    * Adds show_more to asset pipeline
    
    * Adds URLs on default JSON config
    
    * Adds URLs on default JSON config
    
    * Fix rubocop warnings
    
    * Adds sane defaults on petition form (with comment on override)

--- app/forms/decidim/petitions/admin/petition_form.rb
+++ app/forms/decidim/petitions/admin/petition_form.rb
@@ -11,73 +11,13 @@ module Decidim
         translatable_attribute :description, String
         translatable_attribute :instructions_url, String
         attribute :image
-        attribute :json_schema, SchemaAttribute, default: {
-          "mandatory": [
-            {
-              "name": {
-                "ca": "Credencial per participar",
-                "en": "Credential to participate",
-                "es": "Credencial para participar"
-              },
-              "provenance": {
-                "url": "https://credential-test.dyne.org",
-                "issuerName": {
-                  "ca": "Gestor de credencials DECODE",
-                  "en": "DECODE Credential Issuer",
-                  "es": "Gestor de credenciales de DECODE"
-                },
-                "petitionsUrl": "https://petition-test.dyne.org"
-              },
-              "verificationInput": [
-                {
-                  "id": "codes",
-                  "name": {
-                    "ca": "Codi",
-                    "en": "Code",
-                    "es": "Código"
-                  },
-                  "type": "string"
-                }
-              ]
-            }
-          ]
-        }
-
-        # rubocop:disable Style/WordArray
-
-        attribute :json_attribute_info, SchemaAttribute, default: [{
-          "name": "codes",
-          "type": "str",
-          "value_set": [
-            "1234",
-            "a_password"
-          ]
-        }]
-
-        attribute :json_attribute_info_optional, SchemaAttribute, default: [
-          {
-            "k": 2,
-            "name": "age",
-            "type": "str",
-            "value_set": ["0-19", "20-29", "30-39", ">40"]
-          },
-          {
-            "k": 2,
-            "name": "gender",
-            "type": "str",
-            "value_set": ["F", "M", "O"]
-          },
-          {
-            "k": 2,
-            "name": "district",
-            "type": "str",
-            "value_set": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-          }
-        ]
-
-        # rubocop:enable Style/WordArray
-
         attribute :is_reissuable
+
+        # We override these defaults on a helper in the form to add the
+        # credential and petition URLs in the JSON
+        attribute :json_schema, SchemaAttribute, default: {}
+        attribute :json_attribute_info_optional, SchemaAttribute, default: [{}]
+        attribute :json_attribute_info, SchemaAttribute, default: {}
 
         mimic :petition
 
@@ -86,6 +26,7 @@ module Decidim
         validates :image, file_size: {
           less_than_or_equal_to: ->(_record) { Decidim.maximum_attachment_size }
         }, file_content_type: { allow: ["image/jpeg", "image/png"] }
+
       end
     end
   end
---
+++ app/helpers/decidim/petitions/admin/petitions_helper.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Petitions
+    module Admin
+      module PetitionsHelper
+        def default_json_attribute_info
+          # rubocop:disable Style/WordArray
+          [{
+            "name": "codes",
+            "type": "str",
+            "value_set": [
+              "1234",
+              "a_password"
+            ]
+          }]
+        end
+
+        def default_json_attribute_info_optional
+          [
+            {
+              "k": 2,
+              "name": "age",
+              "type": "str",
+              "value_set": ["0-19", "20-29", "30-39", ">40"]
+            },
+            {
+              "k": 2,
+              "name": "gender",
+              "type": "str",
+              "value_set": ["F", "M", "O"]
+            },
+            {
+              "k": 2,
+              "name": "district",
+              "type": "str",
+              "value_set": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+            }
+          ]
+          # rubocop:enable Style/WordArray
+        end
+
+        def default_json_schema
+          credential_url = current_component.settings.credential_issuer_api_url
+          petition_url = current_component.settings.petitions_api_url
+          # rubocop:disable Style/UnneededInterpolation
+          {
+            "mandatory": [
+              {
+                "name": {
+                  "ca": "Credencial per participar",
+                  "en": "Credential to participate",
+                  "es": "Credencial para participar"
+                },
+                "provenance": {
+                  "url": "#{credential_url}",
+                  "issuerName": {
+                    "ca": "Gestor de credencials DECODE",
+                    "en": "DECODE Credential Issuer",
+                    "es": "Gestor de credenciales de DECODE"
+                  },
+                  "petitionsUrl": "#{petition_url}"
+                },
+                "verificationInput": [
+                  {
+                    "id": "codes",
+                    "name": {
+                      "ca": "Codi",
+                      "en": "Code",
+                      "es": "Código"
+                    },
+                    "type": "string"
+                  }
+                ]
+              }
+            ]
+          }
+          # rubocop:enable Style/UnneededInterpolation
+        end
+      end
+    end
+  end
+end
